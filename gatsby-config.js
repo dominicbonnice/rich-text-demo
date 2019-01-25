9eb0b840e83dacb28066bd11fd6e045cd9e24614
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2019-01-25 16:09:14 +0100

    Fix netlify build

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE,
