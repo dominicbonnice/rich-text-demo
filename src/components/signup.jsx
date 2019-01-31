523095f29679580e1d43091eea3155b7742e4262
João <hello@joao.pt>
2019-01-31 11:55:03 +0100

    Add sign-up forms to container

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 export default () => (
-	<form 
-		className="signup" 
-		action="https://contentful.us18.list-manage.com/subscribe/post?u=64719f4e18541c12bab05be89&amp;id=be74f09e93" 
-		method="post" 
-		id="mc-embedded-subscribe-form" 
+	<form
+		className="container signup" 
+		action="https://contentful.us18.list-manage.com/subscribe/post?u=64719f4e18541c12bab05be89&amp;id=be74f09e93"
+		method="post"
+		id="mc-embedded-subscribe-form"
 		name="mc-embedded-subscribe-form"
 		target="_blank">
 		<h2>Participate in the Alpha program</h2>
 		<p>
-			If you want to shape the above features with your feedback, sign up in the mailchimp form below and we'll get in touch with you to enable the alpha features. 
+			If you want to shape the above features with your feedback, sign up in the mailchimp form below and we'll get in touch with you to enable the alpha features.
 		</p>
 		<div>
-			<input 
-				type="email" 
-				placeholder="Your Email address (required)" 
-				name="EMAIL" 
+			<input
+				type="email"
+				placeholder="Your Email address (required)"
+				name="EMAIL"
 				className="signup__input-text"
 				id="mce-EMAIL" />
 			<input
@@ -34,5 +34,3 @@ export default () => (
 		</div>
 	</form>
 );
-
-
